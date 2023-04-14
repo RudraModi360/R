23dc27cafe9abf0ef3699aa8d788608718fa7ef5
RudraModi@360 <rudramodi9560@gmail.com>
2023-04-14 14:17:44 +0530

    shoor bug

--- PYGAME/Level_1.py
+++ PYGAME/Level_1.py
@@ -1,3 +1,95 @@
+# from ursina.scripts.noclip_mode import NoclipMode2d
+# import time
+# from ursina import *
+# from ursina.prefabs.platformer_controller_2d import PlatformerController2d
+
+# window.borderless = False
+# app = Ursina()
+# window.color = color.black10
+# player = PlatformerController2d()
+# player.x = 1
+# player.texture = 'char.png'
+# player.x = raycast(player.world_position, player.right).world_point[1] + .01
+# player.bullet_renderer = Entity(model=Mesh(
+#     mode='point', thickness=.2), texture='circle', color=color.yellow)
+# # ec = EditorCamera(rotation_x=-20)
+
+
+# def shoot():
+#     player.bullet_renderer.model.vertices.append(player.position)
+
+
+# shoot_cooldown = .1
+# shoot_sequence = Sequence(Func(shoot), Wait(shoot_cooldown), loop=True)
+# enemies=[]
+
+# for i, bullet in enumerate(player.bullet_renderer.model.vertices):
+#         player.bullet_renderer.model.vertices[i] += Vec3(0, time.dt * 10, 0)
+#         for enemy in enemies:
+#             if distance_2d(bullet, enemy) < .5:
+#                 enemy.hp -= 1
+#                 enemy.blink(color.white)
+#                 if enemy.hp <= 0:
+#                     enemies.remove(enemy)
+#                     destroy(enemy)
+#                     # todo: add explosion particles and sound effect
+#                 player.bullet_renderer.model.vertices.remove(bullet)
+
+#                 print('a')
+
+# if len(player.bullet_renderer.model.vertices):
+#         player.bullet_renderer.model.vertices = player.bullet_renderer.model.vertices[-100:]  # max bullets
+
+#         player.bullet_renderer.model.generate()
+
+
+# def input(key):
+#     if key == 'space':
+#         shoot_sequence.start()
+#     if key == 'space up':
+#         shoot_sequence.paused = True
+
+
+
+
+# enemies = []
+# enemy = Entity(model=Circle(3), texture='2.png', position=(0, 16), z=1, speed=1, hp=5)
+# enemies.append(enemy)
+
+# def enemy_update():
+#     for e in enemies:
+#         e.position += e.up * enemy.speed * time.dt
+
+
+
+# enemy_handler = Entity(update=enemy_update)
+
+
+# bg = Entity(model='quad', texture='bg2.png', scale=(45, 10, 1), y=3)
+# duplicate(bg,x=40)
+# ground = Entity(model='cube', color=(67, 30, 67, 0.3), z=-.1, y=-1,origin_y=.5, scale=(100, 10, 1), collider='box', ignore=True)
+# camera.orthographic = True
+# camera.fov = 20
+
+# wall = Draggable(model='cube', color=color.white33, origin=(0,.5), scale=(5,10), x=10, y=.5, collider='box')
+
+# celling= Entity(model='cube', color=color.white33, origin_y=.3,scale=(8,1,1),y=2,x=10, collider='box')
+# ceiling = Entity(model='cube', color=color.white33,origin_y=.5, scale=(10, 1, 1), y=4, collider='box')
+# level=Entity(model='cube',color=color.white50,origin_y=.5,y=4,x=20,scale=(3,1),collider='box')
+
+# camera.add_script(SmoothFollow(target=player, offset=[0, 5, -30], speed=4))
+
+# input_handler.bind('right arrow', 'd')
+# input_handler.bind('left arrow', 'a')
+# input_handler.bind('up arrow', 'space')
+# input_handler.bind('down arrow', 'b')
+# input_handler.bind('gamepad dpad right', 'd')
+# input_handler.bind('gamepad dpad left', 'a')
+# input_handler.bind('gamepad a', 'space')
+
+# player.add_script(NoclipMode2d())
+
+# app.run()
 from ursina.scripts.noclip_mode import NoclipMode2d
 import time
 from ursina import *
@@ -6,6 +98,56 @@ from ursina.prefabs.platformer_controller_2d import PlatformerController2d
 window.borderless = False
 app = Ursina()
 window.color = color.black10
+player = PlatformerController2d()
+player.x = 1
+player.texture = 'char.png'
+player.x = raycast(player.world_position, player.right).world_point[1] + .01
+player.bullet_renderer = Entity(model=Mesh(
+    mode='point', thickness=.2), texture='circle', color=color.yellow)
+player.bullet_renderer.position = player.position  # set the position of the bullet renderer to the player position
+player.bullet_renderer.parent = scene  # add the bullet renderer to the scene
+# ec = EditorCamera(rotation_x=-20)
+
+
+def shoot():
+    player.bullet_renderer.model.vertices.append(player.position)
+
+
+shoot_cooldown = .1
+shoot_sequence = Sequence(Func(shoot), Wait(shoot_cooldown), loop=True)
+
+
+enemies = []
+enemy = Entity(model=Circle(3), texture='2.png', position=(0, 16), z=1, speed=1, hp=5)
+enemies.append(enemy)
+
+def enemy_update():
+    for e in enemies:
+        e.position += e.up * enemy.speed * time.dt
+
+
+def update():
+    shoot_sequence.start()  # start the shoot sequence
+    for i, bullet in enumerate(player.bullet_renderer.model.vertices):
+        player.bullet_renderer.model.vertices[i] += Vec3(0, time.dt * 10, 0)
+        for enemy in enemies:
+            if distance_2d(bullet, enemy) < .5:
+                enemy.hp -= 1
+                enemy.blink(color.white)
+                if enemy.hp <= 0:
+                    enemies.remove(enemy)
+                    destroy(enemy)
+                    # todo: add explosion particles and sound effect
+                player.bullet_renderer.model.vertices.remove(bullet)
+
+                print('a')
+
+    if len(player.bullet_renderer.model.vertices):
+        player.bullet_renderer.model.vertices = player.bullet_renderer.model.vertices[-100:]  # max bullets
+        player.bullet_renderer.model.generate()
+
+
+enemy_handler = Entity(update=enemy_update)
 
 
 bg = Entity(model='quad', texture='bg2.png', scale=(45, 10, 1), y=3)
@@ -13,27 +155,14 @@ duplicate(bg,x=40)
 ground = Entity(model='cube', color=(67, 30, 67, 0.3), z=-.1, y=-1,origin_y=.5, scale=(100, 10, 1), collider='box', ignore=True)
 camera.orthographic = True
 camera.fov = 20
-# ground = Entity(model='cube', color=(67, 30, 67, 0.3), z=-.1, y=-1,origin_y=.5, scale=(100, 10, 1), collider='box', ignore=True)
-# random.seed(4)
-# wall=Entity(model='cube',color=color.white33,origin)oh
-# sprite_texture = load_texture('bg_remove.png')
-# cropped_texture = Texture.subtexture(sprite_texture, ((0, 0), (26,48)),((25,2),(50,48)),((51,6),(78,47)),((79,5),(105,46)),((106,5),(132,47)),((134,6),(160,45)),((162,5),(185,46)) # Crop a 32x32 square from the top-left corner of the image
-# sprite = Sprite(texture=cropped_texture)
 
-#Sprite.look_at_2d(self="bg_remove.png",target="standing",axis=(100,194))
 wall = Draggable(model='cube', color=color.white33, origin=(0,.5), scale=(5,10), x=10, y=.5, collider='box')
 
 celling= Entity(model='cube', color=color.white33, origin_y=.3,scale=(8,1,1),y=2,x=10, collider='box')
 ceiling = Entity(model='cube', color=color.white33,origin_y=.5, scale=(10, 1, 1), y=4, collider='box')
-# level=Entity(model='cube',color=color.white50,scale=(3,1),collider='box')
-
-player = PlatformerController2d()
-player.x = 1
-player.texture = 'char.png'
-player.y = raycast(player.world_position, player.down).world_point[1] + .01
+level=Entity(model='cube',color=color.white50,origin_y=.5,y=4,x=20,scale=(3,1),collider='box')
 
 camera.add_script(SmoothFollow(target=player, offset=[0, 5, -30], speed=4))
-
 
 input_handler.bind('right arrow', 'd')
 input_handler.bind('left arrow', 'a')
@@ -42,8 +171,6 @@ input_handler.bind('down arrow', 'b')
 input_handler.bind('gamepad dpad right', 'd')
 input_handler.bind('gamepad dpad left', 'a')
 input_handler.bind('gamepad a', 'space')
-
-
 
 player.add_script(NoclipMode2d())
 
